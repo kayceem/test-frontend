907b732163357ba5291f8059e7e7e7832313d569
Quoc Trung <trungqt1905@gmail.com>
2024-01-19 16:32:22 +0700

    ELP-139-diều-chỉnh-ui-trang-enroll-thanh-cong-them-footer-content-header
    ELP-139-diều-chỉnh-ui-trang-enroll-thanh-cong-them-footer-content-header @sangtrandev00

--- src/pages/site/SubscribeCourse/index.tsx
+++ src/pages/site/SubscribeCourse/index.tsx
@@ -1,5 +1,6 @@
-import { Button } from 'antd';
+import { Button, Result } from 'antd';
 import { Link, useParams } from 'react-router-dom';
+import { CheckCircleOutlined, SmileOutlined } from '@ant-design/icons';
 import './SubscribeCourse.scss';
 
 const SubsribeCourse = () => {
@@ -7,11 +8,18 @@ const SubsribeCourse = () => {
 
   return (
     <div className='subscribe-course'>
-      <div className='subscribe-course__wrapper container bg-slate-300'>
-        <h2 className='subscribe-course__title'>Subscribe Course {courseId} sucessfully!</h2>
-        <Link to={`/path-player/?courseId=${courseId as string}`}>
-          <Button className='subscribe-course__btn'>Go to course now </Button>
-        </Link>
+      <div className='subscribe-course__wrapper container bg-slate-300 rounded-2xl'>
+        <div className='py-16'>
+          <Result className='reset-margin p-0' icon={<CheckCircleOutlined />} />
+          <div className='flex flex-col	items-center'>
+            <h2 className='subscribe-course__title uppercase text-6xl'>Subscribe Course successfully</h2>
+            <Link to={`/path-player/?courseId=${courseId as string}`}>
+              <Button type='primary' className='text-2xl mt-6'>
+                Start course
+              </Button>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
