0753be891c332adfb4474d49671e5a200cb6382f
Quoc Trung <trungqt1905@gmail.com>
2024-02-23 21:25:25 +0700

    ELP-200-fix-bug-chức-nang-da-lam-trung
    Điều chỉnh margin Blog ở trang chủ + fix lỗi login bằng github + Sửa UI trang Blog và Blog-Detail + Create thảo luận khóa học

--- src/pages/site/Blog/components/CommentForm/CommentForm.tsx
+++ src/pages/site/Blog/components/CommentForm/CommentForm.tsx
@@ -1,30 +1,83 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import React, { useState } from 'react';
 import './CommentForm.scss';
 import Avatar from 'antd/es/avatar/avatar';
 import { UserOutlined } from '@ant-design/icons';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css'; // import styles
-import { Button } from 'antd';
+import { Button, message, notification } from 'antd';
+import { useAddBlogCommentMutation } from '../../../client.service';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../../../store/store';
 
-const CommentForm: React.FC = () => {
+function htmlToText(htmlString: string) {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(htmlString, 'text/html');
+  return doc.body.textContent || '';
+}
+
+const CommentForm: React.FC<{ blogId: string; commentLength: number }> = ({ blogId, commentLength }) => {
   const [comment, setComment] = useState('');
+  const [error, setError] = useState('');
+  const [addComment, { isLoading }] = useAddBlogCommentMutation();
+  const userId = useSelector((state: RootState) => state.auth.userId);
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleContentChange = (content: string) => {
+    setComment(content);
+    if (!content.trim()) {
+      setError('Vui lòng nhập nội dung bình luận.');
+    } else {
+      setError('');
+    }
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    setComment('');
+    if (!htmlToText(comment).trim()) {
+      setError('Vui lòng nhập nội dung bình luận.');
+      return;
+    }
+    setError('');
+    try {
+      await addComment({ blogId, content: htmlToText(comment), userId });
+      setComment('');
+      notification.success({
+        message: 'Thành công',
+        description: 'Bình luận đã được thêm thành công.',
+        duration: 3
+      });
+    } catch (error) {
+      notification.error({
+        message: 'Thất bại',
+        description: 'Đã xảy ra lỗi khi thêm bình luận.',
+        duration: 3
+      });
+    }
   };
 
   return (
     <form className='comment-form' onSubmit={handleSubmit}>
-      <div className='div flex mb-36'>
-        <Avatar className='mr-4' icon={<UserOutlined />} />
-        <ReactQuill className='comment-input' placeholder='Thêm bình luận...' value={comment} onChange={setComment} />
+      <div className='comment-title text-3xl mb-6 ml-14'>{commentLength} bình luận</div>
+      <div className='div flex mb-20'>
+        <Avatar src='https://api.dicebear.com/7.x/miniavs/svg?seed=1' className='mr-4 text-3xl' />{' '}
+        <ReactQuill
+          className='comment-input'
+          placeholder='Thêm bình luận...'
+          value={comment}
+          onChange={handleContentChange}
+        />
       </div>
+      {error && (
+        <div className='mt-9 ml-16' style={{ color: 'red', fontSize: '14px' }}>
+          {error}
+        </div>
+      )}
       <div className='div flex justify-end'>
-        <Button className='comment-btn mr-8' type='primary' htmlType='submit'>
+        <Button className='comment-btn mr-8' type='primary' htmlType='submit' disabled={isLoading}>
           Bình luận
         </Button>
-        <Button className='comment-btn' type='primary' htmlType='submit'>
+        <Button className='comment-btn' type='default' onClick={() => setComment('')} disabled={isLoading}>
           Hủy
         </Button>
       </div>
--- src/pages/site/Blog/components/SidingModal/SlidingModal.tsx
+++ src/pages/site/Blog/components/SidingModal/SlidingModal.tsx
@@ -1,53 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import './SidingModal.scss';
-import CommentList from '../CommentList/CommentList';
+import React, { useEffect, useState } from 'react';
+import { Drawer } from 'antd';
 import CommentForm from '../CommentForm/CommentForm';
+import CommentList from '../CommentList/CommentList';
+import { BlogComment } from '../../../../../types/blogComments.type';
+import { useGetBlogCommentsQuery } from '../../../client.service';
 
 interface SlidingModalProps {
   isOpen: boolean;
   onClose: () => void;
+  blogId: string;
 }
 
-const SlidingModal: React.FC<SlidingModalProps> = ({ isOpen, onClose }) => {
-  const [visible, setVisible] = useState(isOpen);
+const SlidingModal: React.FC<SlidingModalProps> = ({ isOpen, onClose, blogId }) => {
+  const [commentsData, setCommentsData] = useState<BlogComment[]>([]);
+  const { data, error, isLoading } = useGetBlogCommentsQuery(blogId);
+  const [commentLength, setCommentLength] = useState(0);
+
+  // Cập nhật state khi dữ liệu bình luận thay đổi
+  useEffect(() => {
+    if (data && data) {
+      setCommentsData(data.comments);
+    }
+  }, [data]);
 
   useEffect(() => {
-    setVisible(isOpen);
-  }, [isOpen]);
-
-  const handleClose = () => {
-    setVisible(false);
-    setTimeout(onClose, 300); // Assuming 300ms is your animation duration
-  };
-
-  const commentsData = [
-    {
-      id: '1',
-      author: 'khadev27',
-      content: 'Rất chi tiết cám ơn bạn nha'
+    if (commentsData.length > 0) {
+      setCommentLength(commentsData.length);
     }
-  ];
+  }, [commentsData]);
 
   return (
-    <div className={`modal ${visible ? 'open' : 'closed'}`}>
-      <div className='px-8 py-8 modal-content'>
-        <button className='text-3xl opacity-60 mb-6' onClick={handleClose}>
-          X
-        </button>
-        <div className='modal-header'>
-          <div className='modal-title text-3xl'>Chưa có bình luận</div>
-        </div>
-        <form className='comment-input-form'>
-          <button className='comment-btn' type='submit'>
-            <i className='fa fa-paper-plane'></i>
-          </button>
-          <div className='comments-container'>
-            <CommentForm />
-            <CommentList comments={commentsData} />
-          </div>
-        </form>
+    <Drawer
+      title={commentsData.length > 0 ? 'Bình luận' : 'Chưa có bình luận'}
+      placement='right'
+      onClose={onClose}
+      visible={isOpen}
+      width={720}
+    >
+      <div className='comment-input-form' style={{ marginBottom: 16 }}>
+        <CommentForm blogId={blogId} commentLength={commentLength} />
+        <CommentList comments={commentsData} blogId={blogId} />
       </div>
-    </div>
+    </Drawer>
   );
 };
 
