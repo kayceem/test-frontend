451becabacf597f15f58309f223ca8ecd4eb02cd
Le Van Hieu <hieulv.2305@gmail.com>
2024-02-24 02:14:04 +0700

    done breakcrumb reviews

--- src/pages/admin/Reviews/Reviews.tsx
+++ src/pages/admin/Reviews/Reviews.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReviewsTable from './ReviewsTable/ReviewsTable';
-
+import { Breadcrumb } from 'antd';
+import { Link } from 'react-router-dom';
 const Reviews: React.FC = () => {
   return (
     <div>
+      <div className='breakcrumb'>
+        <Breadcrumb
+          items={[
+            {
+              title: 'Reviews'
+            },
+            {
+              title: <Link to='#'>Reviews Manager</Link>
+            }
+          ]}
+        />
+      </div>
       <ReviewsTable />
     </div>
   );
