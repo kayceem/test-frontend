964b74b112192455ae93e6ae99cc570de0ebae0e
Le Van Hieu <hieulv.2305@gmail.com>
2024-03-12 20:51:43 +0700

    done develop welcome

--- src/pages/admin/Welcome/welcome.tsx
+++ src/pages/admin/Welcome/welcome.tsx
@@ -1,15 +1,26 @@
+import './welcome.scss';
 type ParamsType = {
   _q: string;
   _page: number;
   _limit: number;
   _status?: string;
 };
-
 const Welcome = () => {
-
   return (
     <div className='welcome'>
-     <h1>Welcome to the admin page</h1>
+      <div className='welcome-header'>
+        <div className='welcome-header-left'>
+          <h3 className='title'>Welcome to Admin! 🎉</h3>
+          <div className='desc'>
+            Welcome back! Wishing you a productive and enjoyable day working on your admin website. Hope you have great
+            experiences and success in all management activities.
+          </div>
+          <div className='heart'></div>
+        </div>
+        <div className='welcome-header-right'>
+          <img src="https://sneat-vuetify-admin-template.vercel.app/assets/illustration-john-light-0061869a.png" alt="" />
+        </div>
+      </div>
     </div>
   );
 };
