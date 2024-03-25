ac571b19da1964494a68df3cd2985e272dbc11d7
Le Van Hieu <hieulv.2305@gmail.com>
2024-03-25 23:00:03 +0700

    fix style form setting

--- src/pages/admin/Settings/index.tsx
+++ src/pages/admin/Settings/index.tsx
@@ -90,90 +90,98 @@ const Settings = () => {
       </Breadcrumb>
       <Card className='mt-2'>
         <Form form={form} layout='vertical' onFinish={onFinish} autoComplete='off' className='form-setting m-auto'>
-          <Form.Item label='Avatar' name='avatar'>
+          <Form.Item className='setting-avatar' label='Avatar' name='avatar'>
+            {/* <img src={avatarUrl} alt='Avatar' style={{ width: '100px' }} /> */}
             <Upload beforeUpload={() => false} onChange={handleChange} multiple={false} fileList={fileList}>
               <Button icon={<UploadOutlined style={{ color: '#000' }} />}>Select Image</Button>
             </Upload>
           </Form.Item>
-          <div className='setting-group'>
-            <Form.Item
-              className='setting-group-item'
-              label='Name'
-              name='name'
-              rules={[{ required: true, message: 'Please input your name!' }]}
-            >
-              <Input placeholder='Enter your full name' />
-            </Form.Item>
-            <Form.Item
-              className='setting-group-item'
-              label='Username'
-              name='username'
-              rules={[{ required: true, message: 'Please input your username!' }]}
-            >
-              <Input placeholder='Enter your username' />
-            </Form.Item>
+          <div className='setting-dlex'>
+            <div className='setting-item'>
+              <div className='setting-group'>
+                <Form.Item
+                  className='setting-group-item'
+                  label='Name'
+                  name='name'
+                  rules={[{ required: true, message: 'Please input your name!' }]}
+                >
+                  <Input placeholder='Enter your full name' />
+                </Form.Item>
+                <Form.Item
+                  className='setting-group-item'
+                  label='Username'
+                  name='username'
+                  rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                  <Input placeholder='Enter your username' />
+                </Form.Item>
+              </div>
+              <div className='setting-group'>
+                <Form.Item
+                  className='setting-group-item'
+                  label='Email'
+                  name='email'
+                  rules={[{ type: 'email', message: 'The input is not a valid E-mail!' }]}
+                >
+                  <Input placeholder='Enter your email' />
+                </Form.Item>
+                <Form.Item
+                  className='setting-group-item'
+                  label='Phone'
+                  name='phone'
+                  rules={[{ required: true, message: 'Please input your phone number!' }]}
+                >
+                  <Input placeholder='Enter your phone' />
+                </Form.Item>
+              </div>
+              <div className='setting-group'>
+                <Form.Item label='Headline' name='headline' className='setting-group-item'>
+                  <Input placeholder='Enter your headline' />
+                </Form.Item>
+                <Form.Item label='Website' name='website' className='setting-group-item'>
+                  <Input placeholder='Enter your website (URL)' />
+                </Form.Item>
+              </div>
+              <Form.Item
+                label='Language'
+                name='language'
+                rules={[{ required: true, message: 'Please select your language!' }]}
+              >
+                <Select placeholder='Select a language'>
+                  <Option value='en'>English</Option>
+                  <Option value='es'>Spanish</Option>
+                  <Option value='pt'>Portuguese</Option>
+                  <Option value='it'>Italian</Option>
+                  <Option value='de'>German</Option>
+                  <Option value='fr'>French</Option>
+                </Select>
+              </Form.Item>
+            </div>
+            <div className='setting-item'>
+              <Form.Item label='Biography' name='biography'>
+                <TextArea rows={5} placeholder='Enter your biography' />
+              </Form.Item>
+              {/* Social Media Links */}
+              <div className='setting-group'>
+                <Form.Item label='Twitter' name='twitter' className='setting-group-item'>
+                  <Input placeholder='Enter your twitter (http://twitter.com/)' />
+                </Form.Item>
+                <Form.Item label='Facebook' name='facebook' className='setting-group-item'>
+                  <Input placeholder='Enter your facebook (http://facebook.com/)' />
+                </Form.Item>
+              </div>
+              <div className='setting-group'>
+                <Form.Item label='LinkedIn' name='linkedin' className='setting-group-item'>
+                  <Input placeholder='Enter your linkedin (http://linkedin.com/in/)' />
+                </Form.Item>
+                <Form.Item label='YouTube' name='youtube' className='setting-group-item'>
+                  <Input placeholder='Enter your youtube (http://youtube.com/)' />
+                </Form.Item>
+              </div>
+
+            </div>
           </div>
-          <div className='setting-group'>
-            <Form.Item
-              className='setting-group-item'
-              label='Email'
-              name='email'
-              rules={[{ type: 'email', message: 'The input is not a valid E-mail!' }]}
-            >
-              <Input placeholder='Enter your email' />
-            </Form.Item>
-            <Form.Item
-              className='setting-group-item'
-              label='Phone'
-              name='phone'
-              rules={[{ required: true, message: 'Please input your phone number!' }]}
-            >
-              <Input placeholder='Enter your phone' />
-            </Form.Item>
-          </div>
-          <div className='setting-group'>
-            <Form.Item label='Headline' name='headline' className='setting-group-item'>
-              <Input placeholder='Enter your headline' />
-            </Form.Item>
-            <Form.Item label='Website' name='website' className='setting-group-item'>
-              <Input placeholder='Enter your website (URL)' />
-            </Form.Item>
-          </div>
-          <Form.Item label='Biography' name='biography'>
-            <TextArea rows={4} placeholder='Enter your biography' />
-          </Form.Item>
-          {/* Social Media Links */}
-          <div className='setting-group'>
-            <Form.Item label='Twitter' name='twitter' className='setting-group-item'>
-              <Input placeholder='Enter your twitter (http://twitter.com/)' />
-            </Form.Item>
-            <Form.Item label='Facebook' name='facebook' className='setting-group-item'>
-              <Input placeholder='Enter your facebook (http://facebook.com/)' />
-            </Form.Item>
-          </div>
-          <div className='setting-group'>
-            <Form.Item label='LinkedIn' name='linkedin' className='setting-group-item'>
-              <Input placeholder='Enter your linkedin (http://linkedin.com/in/)' />
-            </Form.Item>
-            <Form.Item label='YouTube' name='youtube' className='setting-group-item'>
-              <Input placeholder='Enter your youtube (http://youtube.com/)' />
-            </Form.Item>
-          </div>
-          <Form.Item
-            label='Language'
-            name='language'
-            rules={[{ required: true, message: 'Please select your language!' }]}
-          >
-            <Select placeholder='Select a language'>
-              <Option value='en'>English</Option>
-              <Option value='es'>Spanish</Option>
-              <Option value='pt'>Portuguese</Option>
-              <Option value='it'>Italian</Option>
-              <Option value='de'>German</Option>
-              <Option value='fr'>French</Option>
-            </Select>
-          </Form.Item>
-          <Form.Item>
+          <Form.Item className='setting-btn'>
             <Button type='primary' htmlType='submit'>
               Save Changes
             </Button>
